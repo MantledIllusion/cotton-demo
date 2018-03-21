aa21b982dc3b8e25fb1582053c59b85a7fa65e08
tobias.weber <tobias.weber@ecotel.de>
2018-03-21 12:58:53 +0100

    Added query param tutorial

--- src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomeView.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomeView.java
@@ -1,5 +1,7 @@
 package com.mantledillusion.vaadin.cotton.demo.view;
 
+import com.mantledillusion.vaadin.cotton.QueryParam;
+import com.mantledillusion.vaadin.cotton.RequiredQueryParam;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Addressed;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Addressed.Redirect;
 import com.mantledillusion.vaadin.cotton.viewpresenter.View;
@@ -12,13 +14,16 @@ import com.vaadin.ui.Label;
 public class HomeView extends View {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@RequiredQueryParam(value="name", valueCount=1, matcher="\\D{2,10}", forced=true, defaultValues={"Visitor"})
+	private QueryParam param;
 
 	@Override
 	protected Component buildUI(TemporalActiveComponentRegistry reg) throws Throwable {
 		HorizontalLayout layout = new HorizontalLayout();
 		layout.setSizeFull();
 		
-		Label label = new Label("Home View");
+		Label label = new Label("Welcome, "+this.param.getValue(0));
 		layout.addComponent(label);
 		layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
 		
