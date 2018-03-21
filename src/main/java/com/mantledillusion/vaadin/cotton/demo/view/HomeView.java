83309620e312f9dd90e85952edec22fbeee0c886
tobias.weber <tobias.weber@ecotel.de>
2018-03-21 15:02:01 +0100

    Added localization tutorial

--- src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomeView.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomeView.java
@@ -1,27 +1,49 @@
 package com.mantledillusion.vaadin.cotton.demo.view;
 
+import java.util.Locale;
+
+import com.mantledillusion.vaadin.cotton.WebEnv;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Addressed;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Addressed.Redirect;
+import com.mantledillusion.vaadin.cotton.viewpresenter.Presented;
 import com.mantledillusion.vaadin.cotton.viewpresenter.View;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 @Addressed(value="CottonDemo/Home", redirects={@Redirect("")})
+@Presented(HomePresenter.class)
 public class HomeView extends View {
 	
 	private static final long serialVersionUID = 1L;
 
+	static final String BTN_COMPONENT_ID = "btn";
+	
 	@Override
 	protected Component buildUI(TemporalActiveComponentRegistry reg) throws Throwable {
-		HorizontalLayout layout = new HorizontalLayout();
-		layout.setSizeFull();
+		HorizontalLayout mainLayout = new HorizontalLayout();
+		mainLayout.setSizeFull();
 		
-		Label label = new Label("Home View");
-		layout.addComponent(label);
-		layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+		VerticalLayout layout = new VerticalLayout();
+		mainLayout.addComponent(layout);
+		mainLayout.setComponentAlignment(layout, Alignment.MIDDLE_CENTER);
 		
-		return layout;
+		Label l = new Label(WebEnv.localize("welcome.message", ", dear Visitor!"));
+		layout.addComponent(l);
+		layout.setComponentAlignment(l, Alignment.TOP_CENTER);
+		
+		Button b = new Button(getOtherLocale().getDisplayName());
+		reg.registerActiveComponent(BTN_COMPONENT_ID, b);
+		layout.addComponent(b);
+		layout.setComponentAlignment(b, Alignment.BOTTOM_CENTER);
+		
+		return mainLayout;
+	}
+	
+	Locale getOtherLocale() {
+		return WebEnv.getCurrentLocale().getISO3Language().equals(Locale.US.getISO3Language()) ? Locale.FRANCE : Locale.US;
 	}
 }
