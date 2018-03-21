821e08f3767d70d92be99a3f79e3f1d0b28c315a
tobias.weber <tobias.weber@ecotel.de>
2018-03-21 10:10:43 +0100

    Added component event handling tutorial

--- src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomeView.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomeView.java
@@ -1,27 +1,40 @@
 package com.mantledillusion.vaadin.cotton.demo.view;
 
 import com.mantledillusion.vaadin.cotton.viewpresenter.Addressed;
+import com.mantledillusion.vaadin.cotton.viewpresenter.Presented;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Addressed.Redirect;
 import com.mantledillusion.vaadin.cotton.viewpresenter.View;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 
 @Addressed(value="CottonDemo/Home", redirects={@Redirect("")})
+@Presented(HomePresenter.class)
 public class HomeView extends View {
 	
 	private static final long serialVersionUID = 1L;
 
+	static final String BTN_COMPONENT_ID = "btn";
+	
+	private Button b;
+	private int count = 0;
+	
 	@Override
 	protected Component buildUI(TemporalActiveComponentRegistry reg) throws Throwable {
 		HorizontalLayout layout = new HorizontalLayout();
 		layout.setSizeFull();
 		
-		Label label = new Label("Home View");
-		layout.addComponent(label);
-		layout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+		this.b = new Button("Unpressed");
+		reg.registerActiveComponent(BTN_COMPONENT_ID, this.b);
+		layout.addComponent(b);
+		layout.setComponentAlignment(b, Alignment.MIDDLE_CENTER);
 		
 		return layout;
+	}
+	
+	void increment() {
+		this.count++;
+		this.b.setCaption("Pressed "+this.count+" time(s)");
 	}
 }
