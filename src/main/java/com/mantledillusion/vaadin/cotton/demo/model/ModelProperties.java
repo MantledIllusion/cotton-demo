0def90508fcb08a669d78d3a6a8432b6b0aa6600
tobias.weber <tobias.weber@ecotel.de>
2018-04-19 13:36:26 +0200

    Adapted to EpiPhy V2

--- src/main/java/com/mantledillusion/vaadin/cotton/demo/model/ModelProperties.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/model/ModelProperties.java
@@ -1,19 +1,19 @@
 package com.mantledillusion.vaadin.cotton.demo.model;
 
-import com.mantledillusion.data.epiphy.DefiniteModelProperty;
 import com.mantledillusion.data.epiphy.ModelProperty;
 import com.mantledillusion.data.epiphy.ModelPropertyList;
+import com.mantledillusion.data.epiphy.ReadOnlyModelProperty;
 
 public interface ModelProperties {
 
-	public static final DefiniteModelProperty<Model, Model> MODEL = ModelProperty.rootChild("model");
+	public static final ReadOnlyModelProperty<Model, Model> MODEL = ModelProperty.rootChild("model");
 
 	public static final ModelPropertyList<Model, SubObject> SUBOBJECTLIST = MODEL.registerChildList("subObjects",
 			model -> model.getSubObjects(), (model, subObjects) -> model.setSubObjects(subObjects));
 
-	public static final DefiniteModelProperty<Model, SubObject> SUBOBJECT = SUBOBJECTLIST
+	public static final ModelProperty<Model, SubObject> SUBOBJECT = SUBOBJECTLIST
 			.defineElementAsChild("subObject");
 
-	public static final DefiniteModelProperty<Model, String> STRINGVALUE = SUBOBJECT.registerChild("stringValue",
+	public static final ModelProperty<Model, String> STRINGVALUE = SUBOBJECT.registerChild("stringValue",
 			subObject -> subObject.getStringValue(), (subObject, stringValue) -> subObject.setStringValue(stringValue));
 }
