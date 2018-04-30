62efc2275a39339a124d757b0eb545ccf1663ed8
Tobias Weber <weber.tobias@live.de>
2018-04-30 18:54:39 +0200

    Adapted to Cotton 1.0.0.ALPHA10

--- src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomePresenter.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/demo/view/HomePresenter.java
@@ -1,9 +1,9 @@
 package com.mantledillusion.vaadin.cotton.demo.view;
 
-import com.mantledillusion.vaadin.cotton.CottonUI.NavigationEvent;
-import com.mantledillusion.vaadin.cotton.CottonUI.NavigationType;
 import com.mantledillusion.vaadin.cotton.QueryParam;
 import com.mantledillusion.vaadin.cotton.RequiredQueryParam;
+import com.mantledillusion.vaadin.cotton.environment.events.navigation.NavigationEvent;
+import com.mantledillusion.vaadin.cotton.environment.events.navigation.NavigationType;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Listen;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Listen.ActiveComponent;
 import com.mantledillusion.vaadin.cotton.viewpresenter.Presenter;
